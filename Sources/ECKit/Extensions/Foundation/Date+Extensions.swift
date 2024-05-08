f7dedf9d94afbac63c563f732e26ed4246a6f549
Evan Cooper <evan.cooper@rogers.com>
2024-05-08 19:27:20 -0400

    date extension

--- Sources/ECKit/Extensions/Foundation/Date+Extensions.swift
+++ Sources/ECKit/Extensions/Foundation/Date+Extensions.swift
@@ -12,4 +12,10 @@ public extension Date {
     func encodedToString(with formatter: DateFormatter = .dateDashed) -> String {
         formatter.string(from: self)
     }
+    
+    var zeroSeconds: Date? {
+        let calendar = Calendar.current
+        let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: self)
+        return calendar.date(from: dateComponents)
+    }
 }
