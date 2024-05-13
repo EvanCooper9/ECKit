47486d90fedc434181e82f4697de52f26f31af99
Evan Cooper <evan.cooper@rogers.com>
2024-05-13 18:22:14 -0400

    update access control

--- Sources/ECKit/UI/AppIcon.swift
+++ Sources/ECKit/UI/AppIcon.swift
@@ -6,6 +6,10 @@ public struct AppIcon: View {
     var size = 60.0
     private var cornerRadius: Double { size * 0.2237 }
 
+    public init(size: Double = 60.0) {
+        self.size = size
+    }
+
     public var body: some View {
         Image(uiImage: Bundle.main.icon)
             .resizable()
